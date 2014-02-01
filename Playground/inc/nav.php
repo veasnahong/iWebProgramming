cb016a584e0ab57ce6df6fbd89b1d21eb157863e
veasnahong <veasnahong@gmail.com>
2014-02-01 12:54:23 -0500

    Basic Webpage

---
+++ Playground/inc/nav.php
@@ -0,0 +1,18 @@
+	<body>
+		<div class="animatedtabs">
+				<ul>
+					<li><a href="index.php" title="Home"><span>Home</span></a></li>
+					<li ><a href="about.php" title="About"><span>About</span></a></li>
+					<li><a href="resume.php" title="Resume"><span>Resume</span></a></li>
+					<li><a href="portfolio.php" title="Portfolio"><span>Portfolio</span></a></li>
+					<li><a href="contact.php" title="Contact"><span>Contact</span></a></li>
+					<li>
+			<form class="navbar-form navbar-left" role="search">
+			  	<div class="form-group">
+			    	<input type="text" class="form-control" placeholder="Search this page">
+			  	</div>					  		
+			</form>
+					</li>
+				</ul>
+			</div>
+	</body>
---
+++ Playground/portfolio.php
@@ -0,0 +1,61 @@
+﻿<!--VeasnaHong's Personal Page -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8"/>
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+	<!-- Everyone gets the default stylesheet -->
+	<link rel="stylesheet" type="text/css" href="/css/styles.css" />
+
+	<!-- Anyone using IE7 and above gets the IE styles too -->
+	<!--[if gte IE 7]>
+	    <link rel="stylesheet" type="text/css" href="/css/ie-overrides.css" />
+	<![endif]--> 
+
+
+<title>Portfolio</title>
+		    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
+		    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css" />
+			<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css" type="text/css" />
+			<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.min.css" type="text/css" />
+			<link rel="stylesheet" href="bootstrap/css/menubar.css" type="text/css" />
+
+
+	<style type="text/css">
+		.auto-style1 
+		{
+			padding: 30px 20px;
+			margin-left:50px;
+			margin-right:50px;	
+			font-family: Verdana, Geneva, Tahoma, sans-serif;
+			color:black;
+		}
+	</style>
+
+</head>
+	<body>
+			
+		<div id="container">
+
+			<div>
+				<h1  class="auto-style1" style="height: 20px">Welcome to Veasna's HomePage</h1>
+			</div>
+
+				<? include 'inc/nav.php';?>	
+
+		<div id="content">
+			<h4> PORTFOLIO </h4>
+		</div>
+	/div>	
+	
+	<div id="footer">
+		Designed and implemented by Veasna Hong. <br/>
+		<a href="mailto:veasnahong@gmail.com?Subject=Ordering%20Request">veasnahong@gmail.com </a>
+	</div>
+			
+</body>
+</html>
