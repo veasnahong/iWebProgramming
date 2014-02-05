b52a82be21cebfe8689fdb35890bea79a96f0105
veasnahong <veasnahong@gmail.com>
2014-02-05 00:37:35 -0500

    Basic Website with NavBar Color

--- BasicWebsite/inc/nav.php
+++ BasicWebsite/inc/nav.php
@@ -1,6 +1,7 @@
-
-<body>
-	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<style type="text/css">
+     .navbar-brand.active { background-color: #000;}
+    </style>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -9,19 +10,35 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">Home</a>
-          <a class="navbar-brand" href="contact.php">Contact</a>
-          <a class="navbar-brand" href="link.php">Link</a>
-          <a class="navbar-brand" href="sale.php">Sale</a>
-          <a class="navbar-brand" href="clearance.php">Clearance</a>
+          <a class="navbar-brand" href="./">Home</a>
         </div>
-        
         <div class="navbar-collapse collapse">
+        
+				<ul class="nav navbar-nav">
+				<li class="contact-menu" ><a href="contact.php">Contact Us</a></li>
+				<li class="links-menu" ><a href="link.php">Link</a></li>
+				<li class="clearance-menu" ><a href="clearance.php">Clearance</a></li>
+				<li class="sale-menu" ><a href="sale.php">Sale</a></li>
+				
+
+			<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Super Menu <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Action</a></li>
+					<li><a href="#">Another action</a></li>
+					<li><a href="#">Something else here</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Separated link</a></li>
+					<li class="divider"></li>
+					<li><a href="#">One more separated link</a></li>
+				</ul>
+			</li>
+			</ul>
+			        
           <form class="navbar-form navbar-right" role="form">
             <div class="form-group">
               <input type="text" placeholder="Email" class="form-control">
             </div>
-            
             <div class="form-group">
               <input type="password" placeholder="Password" class="form-control">
             </div>
@@ -29,5 +46,4 @@
           </form>
         </div><!--/.navbar-collapse -->
       </div>
-   </div>
-</body>
+    </div>
--- BasicWebsite/index.php
+++ BasicWebsite/index.php
@@ -67,5 +67,11 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
+    	<script type="text/javascript">
+	<script type="text/javascript">
+		$(function(){
+		$(".navbar-brand").addClass("active");
+		})
+	</script>
   </body>
 </html>
